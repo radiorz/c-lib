17ecb3f10e65f79a7783fc29fc6d57da6be46c32
tikkhun <s565338107@163.com>
2023-09-10 22:54:46 +0800

    chore: just try wasm

--- example/main.c
+++ example/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "../libhello/hello.h"
+void func(void *arg)
+{
+  int flag = *((int *)arg);
+}
 int main(int argc, char **argv)
 {
   printf("123123\n");
--- wasm/preload.sh
+++ wasm/preload.sh
@@ -1,4 +1,4 @@
-scoop install emscripten
+# scoop install emscripten
 emsdk install latest
 emsdk activate latest
 echo 'source "/mnt/c/Users/tikkhun/scoop/apps/emscripten/3.1.45/emsdk_env.sh"' >> $HOME/.bash_profile
